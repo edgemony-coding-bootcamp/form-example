6fb42758f57345ae27f4175e69504963cef0dec2
Marco Ziliani <marco.ziliani@musement.com>
2021-03-19 12:49:36 +0100

    add form example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: { value: "", modified: false },
+    email: { value: "", modified: false },
+  });
+  function updateData(field) {
+    return function (event) {
+      setFormData({
+        ...formData,
+        [field]: { value: event.target.value, modified: true },
+      });
+    };
+  }
+
+  // updateData('name')
+  // function updateName(event) {
+  //   setFormData({
+  //     ...formData,
+  //     name: { value: event.target.value, modified: true },
+  //   });
+  // };
+
+  // updateData('email')
+  // function updateEmail(event) {
+  //   setFormData({
+  //     ...formData,
+  //     email: { value: event.target.value, modified: true },
+  //   });
+  // };
+
+  function onSubmit(event) {
+    const data = Object.keys(formData).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: formData[key].value,
+      }),
+      {}
+    );
+    console.log(data);
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            value={formData.name.value}
+            name="name"
+            id="name"
+            onChange={updateData("name")}
+            className={formData.name.modified ? "modified" : ""}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={formData.email.value}
+            name="email"
+            id="email"
+            className={formData.email.modified ? "modified" : ""}
+            onChange={updateData("email")}
+            required
+          />
+        </div>
+        <button type="submit">Invia</button>
+      </form>
     </div>
   );
 }
